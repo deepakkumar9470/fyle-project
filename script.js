5cc07bc33a12acc8ac515604deb46ae28d48293e
Deepak Kumar Chouhan <dk397787@gmail.com>
2024-01-18 11:25:58 +0530

    user part done

--- script.js
+++ script.js
@@ -37,33 +37,52 @@ document.addEventListener('DOMContentLoaded', function () {
     function displayUser(user) {
       const userContainer = document.getElementById('user__info');
       const userWrapper = document.getElementsByClassName('user');
-      // const userBio = document.getElementsByClassName('bio');
+      const userBio = document.getElementsByClassName('bio');
       // const location = document.getElementsByClassName('location');
-      // const github = document.getElementsByClassName('github');
-     
-      const userNewBio = document.createElement('div')
-      userContainer.appendChild(userWrapper) 
-      userWrapper.appendChild(userBio)
-      userBio.appendChild(location)
-      userWrapper.appendChild(github)
-      userWrapper.innerHTML = `
-      <img src="${user.avatar_url}" alt="Profile Image" class="profile-image">
+      // const github = document.getElementsByClassName('github');     
+      // const userNewBio = document.createElement('div')
+      // userContainer.appendChild(userWrapper) 
+      // userWrapper.appendChild(userBio)
+      // userBio.appendChild(location)
+      // userWrapper.appendChild(github)
+      userContainer.append(userBio)
+
+      userContainer.innerHTML = `
+      <div class="user">
+          <img style="width:200px;border-radius:50%;border:2px solid #D1D1D1" src="${user.avatar_url}" alt="Profile Image" class="profile-image">
+          <div class="bio">
+             <h2>${user.name}</h2>
+             <p>${user.bio}</p>
+             <div class="location">
+             <img src="./assets/location.svg" 
+             alt="location">
+             <span>${user.location || "Dhanbad"}</span>
+             </div>
+             <span class="twitter">Twitter : ${user.twitter_username || "@dk397787"}</span>
+          </div>
+            </div>
+
+            <div class="github">
+            <img src="./assets/link.svg" alt="">
+            <a href="${user.url}">${user.url}</a>
+            </div>    
+
            `;
 
-        userNewBio.innerHTML = `
-        <h2>${user.name}</h2>
-        <p>${user.bio}</p>
-        <span>${user.twitter_username}</span>
-      `;
+        // userNewBio.innerHTML = `
+        // <h2>${user.name}</h2>
+        // <p>${user.bio}</p>
+        // <span>${user.twitter_username}</span>
+      // `;
 
-      location.innerHTML = `
-        <span>${user.location}</span>
-      `;
-      github.innerHTML = `
+      // location.innerHTML = `
+      //   <span>${user.location}</span>
+      // `;
+      // github.innerHTML = `
 
-        <a href="${user.url}" target="_blank" rel="noopener noreferrer"></a>
+      //   <a href="${user.url}" target="_blank" rel="noopener noreferrer"></a>
         
-      `;
+      // `;
     }
   
     function displayRepos(repos) {
